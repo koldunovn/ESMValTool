a74e2cbeb6dcd8e3d3204850dd1c0b5c8bfd7c4e
Nikolay Koldunov <koldunovn@gmail.com>
2017-06-06 22:49:04 +0200

    first version of eurasian basin

--- diag_scripts/dummy_python_NK.py
+++ diag_scripts/dummy_python_NK.py
@@ -16,7 +16,8 @@
 """
 import inspect
 from esmval_lib import ESMValProject
-
+from netCDF4 import Dataset
+import numpy as np
 import projects
 print(projects)
 
@@ -30,12 +31,14 @@ def main(project_info):
     verbosity = E.get_verbosity()
     plot_type = E.get_graphic_format()
     diag_script = E.get_diag_script_name()
-    print(config_file)
-    print(plot_dir)
-    print(verbosity)
-    print(plot_type)
-    print(diag_script)
-
+    ifile_dict = E.get_raw_inputfile()
+    #print(config_file)
+    #print(plot_dir)
+    #print(verbosity)
+    #print(plot_type)
+    #print(diag_script)
+    #print(ifile_dict)
+    #print(project_info)
       
     print('Hello, here is the dummy routine from the direct python interface!')
 
@@ -43,10 +46,43 @@ def main(project_info):
     #E = ESMValProject(project_info)
 
     # get filenames of preprocessed climatological mean files
-    model_filenames = E.get_clim_model_filenames(variable='ta', monthly=True)
+    model_filenames = E.get_clim_model_filenames(variable='thetao', monthly=True)
+    for mmodel in model_filenames:
+        print(mmodel)
+        datafile = Dataset(model_filenames[mmodel])
+        lon = datafile.variables['lon'][:]
+        lat = datafile.variables['lat'][:]
+        lev = datafile.variables['lev'][:]
+        lon2d, lat2d = lon, lat
+        
+        indi, indj = np.where((lon2d>-60) & (lon2d<100) & (lat2d>80))
+        indi2, indj2 = np.where((lon2d>100) & (lon2d<140) & (lat2d>66))
+
+        indexesi = np.hstack((indi, indi2))
+        indexesj = np.hstack((indj, indj2))
+
+        #print(lon[indexesi, indexesj])
+        print(lev)
+        oce_kpp_euro = np.zeros((lev[0:28].shape[0], 24))
+        for mon in range(24):
+            print(mon)
+            for ind, depth in enumerate(lev[0:28]):
+                level_pp = datafile.variables['thetao'][mon, ind, :, :]
+                oce_kpp_euro[ind,mon] = np.nanmean(level_pp[indexesi, indexesj])
+
+        np.save('oce_with_brine_{}'.format(mmodel), oce_kpp_euro)
+        datafile.close()
+
+        
+        #print(lon)
+        #print(lat)
+
+
     print(model_filenames)
 
     print('Do something here!')
     print('ENDED SUCESSFULLY!!')
+    #print(dir(E))
+
     print('')
 
